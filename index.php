b93f5cf94cd9b14cb8641aafd45d82225043a164
b21217885 <hana.badjoudj@etu.univ-amu.fr>
2024-10-09 14:50:39 +0200

    mention legales

--- index.php
+++ index.php
@@ -2,10 +2,18 @@
 require 'includes/Autoloader.php';
 
 Autoloader::autoload('Accueil');
+Autoloader::autoload('MentionsLegalesController');
 
 $db = new Database('intraiut_1');
 
-
+if (isset($_GET['page']) && $_GET['page'] === 'mentions_legales') {
+    $controller = new MentionsLegalesController();
+    $controller->afficherPage();
+} else {
+    $accueil = new Accueil();
+    $accueil->afficher();
+}
+?>
 
 
 
