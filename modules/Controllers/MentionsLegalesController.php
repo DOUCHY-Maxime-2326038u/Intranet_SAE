758680ff67747e685f0fa71cd97e6ee198df668e
b21217885 <hana.badjoudj@etu.univ-amu.fr>
2024-10-09 16:57:32 +0200

    j'ai enlevé les commentaires

--- modules/Controllers/MentionsLegalesController.php
+++ modules/Controllers/MentionsLegalesController.php
@@ -3,10 +3,9 @@ namespace modules\Controllers;
 
 class MentionsLegalesController
 {
-    // Cette méthode va afficher la page des mentions légales
+
     public function afficherPage()
     {
-        // Inclure la vue des mentions légales
         include_once 'view/mentions_legales.php';
     }
 }
