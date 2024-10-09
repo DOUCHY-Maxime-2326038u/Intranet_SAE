323ecb59cd0cec18d97e3b43bd937ed4b79f5e65
DOUCHY-Maxime-2326038u <146344643+DOUCHY-Maxime-2326038u@users.noreply.github.com>
2024-10-09 16:50:30 +0200

    architechture merge

--- modules/Controllers/MentionsLegalesController.php
+++ modules/Controllers/MentionsLegalesController.php
@@ -1,9 +1,14 @@
 <?php
-class MentionsLegalesController {
+namespace modules\Controllers;
+
+class MentionsLegalesController
+{
     // Cette méthode va afficher la page des mentions légales
-    public function afficherPage() {
+    public function afficherPage()
+    {
         // Inclure la vue des mentions légales
         include_once 'view/mentions_legales.php';
     }
 }
+
 ?>
---
+++ modules/Controllers/PresentationFiliereController.php
@@ -0,0 +1,12 @@
+<?php
+namespace modules\Controllers;
+
+class PresentationFiliereController
+{
+    public function afficherPage()
+    {
+        include_once 'view/presentation_filiere.php';
+    }
+}
+
+?>
