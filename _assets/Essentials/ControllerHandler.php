90b4442b62361011891cc9a4e80d4e681e6efb3f
Batman <joud.chebira@etu.univ-amu.fr>
2024-10-09 16:38:25 +0200

    ajout autoloader / rooter et arborescence : à améliorer

---
+++ _assets/Essentials/ControllerHandler.php
@@ -0,0 +1,38 @@
+<?php
+
+final class ControllerHandler
+{
+    private array $url;
+
+    public function __construct(?string $S_controller, ?string $S_action)
+    {
+        $this->url['controller'] = empty($S_controller)
+            ? 'AccueilController'
+            : ucfirst($S_controller) . 'Controller';
+
+        $this->url['action'] = empty($S_action)
+            ? 'defaultAction'
+            : $S_action . 'Action';
+    }
+
+    public function getUrl(): array
+    {
+        return $this->url;
+    }
+    
+    public function execute(): void
+    {
+        $controller = $this->url['controller'];
+        $action = $this->url['action'];
+
+        if (!class_exists($controller)) {
+            throw new Exception("Le contrôleur '$controller' est introuvable.");
+        }
+
+        if (!method_exists($controller, $action)) {
+            throw new Exception("L'action '$action' est introuvable dans le contrôleur '$controller'.");
+        }
+
+        call_user_func_array([new $controller(), $action], []);
+    }
+}
---
+++ _assets/Essentials/ViewHandler.php
@@ -0,0 +1,27 @@
+<?php
+
+final class ViewHandler
+{
+    public static function bufferStart(): void
+    {
+        ob_start();
+    }
+
+    public static function bufferCollect(): string
+    {
+        return ob_get_clean();
+    }
+
+    public static function show(string $S_loc, array $A_params = []): void
+    {
+        $S_file = Constants::viewsDir() . $S_loc . '.php';
+
+        if (!is_readable($S_file)) {
+            throw new Exception("Fichier de vue non trouvé : " . $S_file);
+        }
+
+        ob_start();
+        include $S_file;
+        ob_end_flush();
+    }
+}
