66df68028cb4f4ba8c239be8c2c4ab0e823dc686
b21217885 <hana.badjoudj@etu.univ-amu.fr>
2024-10-16 14:23:14 +0200

    page à propos

--- modules/Controllers/AproposController.php
+++ modules/Controllers/AproposController.php
@@ -6,6 +6,7 @@ class AproposController
     {
         $params = [
             'page_title' => 'À propos de notre IUT',
+            'description' => 'Le département informatique de l\'IUT d\'Aix-en-Provence forme des informaticiens généralistes...',
         ];
 
         ViewHandler::show('apropos', $params);
--- modules/Views/aPropos.php
+++ modules/Views/aPropos.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>À propos de l'IUT Informatique</title>
+    <title><?= htmlspecialchars($title) ?></title>
     <link rel="stylesheet" href="/_assets/styles/apropos.css">
 </head>
 <body>
