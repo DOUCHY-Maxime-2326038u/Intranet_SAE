e1c7b3ee222a33f50dce1e84476536e1077ff700
DOUCHY-Maxime-2326038u <146344643+DOUCHY-Maxime-2326038u@users.noreply.github.com>
2024-10-18 11:46:09 +0200

    menu intranet avec les onglets

--- modules/Controllers/IntranetController.php
+++ modules/Controllers/IntranetController.php
@@ -13,4 +13,31 @@ final class IntranetController
     {
         return $this -> titre;
     }
+    public function getContentAction()
+    {
+        if (isset($_GET['tab'])) {
+            $tab = $_GET['tab'];
+
+            switch ($tab) {
+                case 'dashboard':
+                    echo "<h2>Tableau de bord</h2><p>Voici le contenu du tableau de bord.</p>";
+                    break;
+                case 'reservation':
+                    echo "<h2>Reservation de salle</h2><p>le contenu de prout.</p>";
+                    break;
+
+                case 'annonce':
+                    echo "<h2>Annonces</h2><p>Les annonces.</p>";
+                    break;
+                case 'professeur':
+                    echo "<h2>Mes professeurs</h2><p>équipe pédagogique avec mail.</p>";
+                    break;
+                default:
+                    echo "<p>Onglet non reconnu.</p>";
+                    break;
+            }
+        } else {
+            echo "<p>Aucun onglet sélectionné.</p>";
+        }
+    }
 }
