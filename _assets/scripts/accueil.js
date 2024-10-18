ffed5e74102e4b8db47efee35c36fd504c3dfe25
SIMON Louis <146358792+SIMON-Louis-2326101aa@users.noreply.github.com>
2024-10-18 09:56:24 +0200

    Ajout js accueil

---
+++ _assets/scripts/accueil.js
@@ -0,0 +1,32 @@
+alert('JavaScript est bien chargé');
+const points = document.querySelectorAll('.event-point');
+const tooltip = document.getElementById('tooltip');
+
+points.forEach(point => {
+    point.addEventListener('mouseenter', function() {
+        // Utilise "info en cours" comme texte par défaut si `data-event` est vide.
+        const eventText = this.dataset.event || 'info en cours';
+        tooltip.textContent = eventText;
+
+        const pointRect = this.getBoundingClientRect();
+        const tooltipRect = tooltip.getBoundingClientRect();
+
+        tooltip.style.left = `${pointRect.left + pointRect.width / 2 - tooltipRect.width / 2}px`;
+        tooltip.style.top = `${pointRect.bottom + window.scrollY + 10}px`;
+
+        tooltip.style.opacity = '1';
+        tooltip.style.transform = 'translateY(0px)';
+    });
+
+    point.addEventListener('mouseleave', function() {
+        tooltip.style.opacity = '0';
+        tooltip.style.transform = 'translateY(-10px)';
+    });
+
+    //Tester si la bulle fonctionne
+    point.addEventListener('mouseenter', function() {
+        console.log('Point survolé');
+        const eventText = this.dataset.event || 'info en cours';
+        tooltip.textContent = eventText;
+    });
+});
