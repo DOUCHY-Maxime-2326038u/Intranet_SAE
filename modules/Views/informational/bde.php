bc973057950aa5ff846840ab3a2cadce16a9e467
DOUCHY-Maxime-2326038u <146344643+DOUCHY-Maxime-2326038u@users.noreply.github.com>
2024-12-12 09:23:18 +0100

    header enlever

--- modules/Views/informational/bde.php
+++ modules/Views/informational/bde.php
@@ -1,18 +1,5 @@
 
 <body>
-<header>
-    <nav class="secondHeader">
-        <ul>
-            <li><a href="/root.php?ctrl=Accueil">Accueil</a></li>
-            <li><a href="/root.php?ctrl=Bde">Bde</a></li>
-            <li><a href="/root.php?ctrl=Apropos">A Propos</a></li>
-            <li><a href="/root.php?ctrl=MentionsLegales">Mentions Légales</a></li>
-            <li><a href="/root.php?ctrl=PresentationFiliere">Présentation des filières</a></li>
-            <li><a href="/root.php?ctrl=Connexion">Intranet</a></li>
-
-        </ul>
-    </nav>
-</header>
 <section class="intro">
     <div class="full-width-container">
         <h2>Découvrez le BDE</h2>
